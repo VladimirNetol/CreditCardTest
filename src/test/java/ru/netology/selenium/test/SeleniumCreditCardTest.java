59000680765c9fc4dbc5acd1be167e444cbcce1e
Vladimir <vladimir_net0logia@mail.ru>
2025-09-14 22:31:14 +0300

    BUILD SUCCESSFUL

---
+++ src/test/java/ru/netology/selenium/test/SeleniumCreditCardTest.java
@@ -0,0 +1,37 @@
+package ru.netology.selenium.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SeleniumCreditCardTest {
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void shouldFormSubmitted() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        //options.addArguments("--headless");
+        ChromeDriver driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Владимир Чернов");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79151231213");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[type = button]")).click();
+        String correctInscription = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", correctInscription.trim());
+    }
+}
